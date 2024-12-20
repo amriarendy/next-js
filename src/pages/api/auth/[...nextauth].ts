9e2e4a3305c83453cf95b080da8bd7db0417f6a1
amriarendy <amriarendy90@gmail.com>
2024-12-21 06:20:48 +0700

    create auth google

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google"
 
 const authOptions: NextAuthOptions = {
     session: {
@@ -7,34 +8,49 @@ const authOptions: NextAuthOptions = {
     },
     secret: process.env.NEXTAUTH_SECRET,
     providers: [
-        CredentialsProvider({
-            type: 'credentials',
-            name: "Credentials",
-            credentials: {
-                email: {label: "Email", type: "email"},
-                fullname: {label: "Full Name", type: "text"},
-                password: { label: "Password", type: "password"}
-            },
-            async authorize(credentials) {
-                const { email, password, fullname} = credentials as {
-                    email: string,
-                    password: string,
-                    fullname: string,
-                };
-                const user: any = { id: 1, email: email, password: password, fullname: fullname, };
-                if (user) {
-                    return user;
-                } else {
-                    return null
-                }
-            }
+        // script auth credentials
+        // CredentialsProvider({
+        //     type: 'credentials',
+        //     name: "Credentials",
+        //     credentials: {
+        //         email: {label: "Email", type: "email"},
+        //         fullname: {label: "Full Name", type: "text"},
+        //         password: { label: "Password", type: "password"}
+        //     },
+        //     async authorize(credentials) {
+        //         const { email, password, fullname} = credentials as {
+        //             email: string,
+        //             password: string,
+        //             fullname: string,
+        //         };
+        //         const user: any = { id: 1, email: email, password: password, fullname: fullname, };
+        //         if (user) {
+        //             return user;
+        //         } else {
+        //             return null
+        //         }
+        //     }
+        // })
+        GoogleProvider({
+            clientId: process.env.GOOGLE_OAUTH_CLIENT_ID || "",
+            clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET || "",
         })
     ],
     callbacks: {
-        jwt({token, account, profile, user}: any) {
+        async jwt({token, account, profile, user}: any) {
             if (account?.provider==="credentials") {
                 token.email = user.email;
                 token.fullname = user.fullname;
+            }
+            if (account?.provider === "google") {
+                const data = {
+                    fullname: user.name,
+                    email: user.email,
+                    image: user.image,
+                    type: "google"
+                }
+                console.log("google auth: ", data);
+                
             }
             return token
         },
--- src/views/auth/login/index.tsx
+++ src/views/auth/login/index.tsx
@@ -1,17 +1,63 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import styles from "./Login.module.scss"
+import { useState } from "react";
+import { signIn } from "next-auth/react";
 
 const LoginViews = () => {
-    const { push } = useRouter();
-    const handleLogin = () => {
-        push("/dashboard");
-    };
+    const [ isLoading, setIsLoading ] = useState(false);
+    const [ error, setError] = useState("");
+    const { push, query } = useRouter();
+    const callbackUrl: any = query.callbackUrl || "/";
+    const handleSubmit = async (event: any) => {
+        event.preventDefault();
+        setError("");
+        setIsLoading(true)
+        try {
+            const res = await signIn("credentials", {
+                email: event.target.email.value,
+                password: event.target.password.value,
+                callbackUrl,
+            });
+            if (!res?.error) {
+                setIsLoading(false);
+                push(callbackUrl);
+            } else {
+                setIsLoading(false);
+                setError("Email or password is incorect!")
+            }
+        } catch (error: any) {
+            setIsLoading(false);
+            setError("Email or password is incorect!")
+        }
+    }
     return ( 
         <div className={styles.login}>
-            <h1 className="text-3xl">Login Page</h1>
-            <button onClick={() => handleLogin()}>Login</button>
-            <p style={{color: "red", border: "1px solid red"}}>Don't have account <Link href={"/auth/register"}>Register</Link></p>
+            <h1 className={styles.login__title}>login Page</h1>
+                {error && <p className={styles.login__error}>{error}</p>}
+                <div className={styles.login__form}>
+                        <form onSubmit={handleSubmit}>
+                            <div className={styles.login__form__item}>
+                                <label htmlFor="email" className={styles.login__form__item__label}>Email</label>
+                                <input id="email" type="email" name="email" placeholder="Email" className={styles.login__form__item__input} />
+                            </div>
+                            <div className={styles.login__form__item}>
+                                <label htmlFor="password" className={styles.login__form__item__label}>Password</label>
+                                <input id="password" type="password" name="password" placeholder="Password" className={styles.login__form__item__input} />
+                            </div>
+                            <button type="submit" className={styles.login__form__item__button} disabled={isLoading}>
+                                {isLoading ? "Loading..." : "Register"}
+                            </button>
+                            <button onClick={()=> signIn("google", {
+                                callbackUrl,
+                                redirect: false
+                            })}
+                            className={styles.login__form__item__google}>
+                                Sign In With Google
+                            </button>
+                        </form>
+                </div>
+                <p className={styles.register__link}>Have already Account? Sign in <Link href={'/auth/login'}>Here</Link></p>
         </div>
     )
 }
